22d3e52930cda35e7cdbb6c5902e0cace8e16b55
Katie <pie10101011@gmail.com>
2023-09-29 02:02:22 +0000

    switched to mock data in json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
+import data from './data.json';
 
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList data={data} />
     </div>
   );
 }
---
+++ src/MockData.js
@@ -0,0 +1,45 @@
+let data = [
+        {
+            "id": "1",
+            "info": {
+                "imageSrc": "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+                "name": "MarginOtto Pizzeria",
+                "address": "1010 Paddington Way",
+                "city": "Flavortown",
+                "state": "NY",
+                "zipCode": "10101",
+                "category": "Italian",
+                "rating": 4.5,
+                "reviewCount": 90
+            }
+        },
+        {
+            "id": "2",
+            "info": {
+                "imageSrc": "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+                "name": "Restaurant Two",
+                "address": "1010 Paddington Way",
+                "city": "Flavortown",
+                "state": "NY",
+                "zipCode": "10101",
+                "category": "Italian",
+                "rating": 4.5,
+                "reviewCount": 90
+            }
+        },
+        {
+            "id": "3",
+            "info": {
+                "imageSrc": "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+                "name": "Pasta Palace",
+                "address": "1010 Paddington Way",
+                "city": "Flavortown",
+                "state": "NY",
+                "zipCode": "10101",
+                "category": "Italian",
+                "rating": 4.5,
+                "reviewCount": 90
+            }
+        }
+    ]
+
