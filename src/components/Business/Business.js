e1906ddfe55d1b694daa4ab8c8e4a94a7dde03fa
Katie <pie10101011@gmail.com>
2023-10-03 23:37:27 +0000

    refactor SearchBar

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -3,27 +3,29 @@ import styles from './Business.module.css';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 
-
-const Business = (props) => {
-    return (
-        <Container className={styles.BusinessCard}>
-        <Card className='m-3'>
-            <div>
-                <img src={props.imageSrc} className="card-img-top img-thumbnail" alt='' />
-            </div>
-            <div className="card-body">
-                <h2 className="Card-title">{props.name}</h2>
-                <p className="card-text p-0 m-0">{props.address}</p>
-                <p className="card-text">{props.city}, {`${props.state} ${props.zipCode}`}</p>
+class Business extends React.Component {
+    render() {
+        return (
+            <Container className={styles.BusinessCard}>
+            <Card className='m-3'>
+                <div>
+                    <img src={this.props.imageSrc} className="card-img-top img-thumbnail" alt='' />
                 </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{props.category}</li>
-                    <li className="list-group-item">{`${props.rating} stars`}</li>
-                    <li className="list-group-item">{`${props.reviewCount} reviews`}</li>
-                </ul>
-        </Card>
-        </Container>
-    );
+                <div className="card-body">
+                    <h2 className="Card-title">{this.props.name}</h2>
+                    <p className="card-text p-0 m-0">{this.props.address}</p>
+                    <p className="card-text">{this.props.city}, {`${this.props.state} ${this.props.zipCode}`}</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">{this.props.category}</li>
+                        <li className="list-group-item">{`${this.props.rating} stars`}</li>
+                        <li className="list-group-item">{`${this.props.reviewCount} reviews`}</li>
+                    </ul>
+            </Card>
+            </Container>
+        );
+    }
+    
 };
 
 export default Business;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,107 +1,222 @@
 import React from 'react';
-import styles from './SearchBar.module.css';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
-import ToggleButton from 'react-bootstrap/ToggleButton';
+import './SearchBar.css';
+import { Form, Container, Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 
-const sortOptions = [
-  { name: 'Best Match', value: 'best_match' },
-  { name: 'Highest Rated', value: 'highest_rating' },
-  { name: 'Most Reviewed', value: 'most_reviewed' }
-];
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-const SearchBar = () => {
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
 
-  const [ sortValue, setSortValue ] = React.useState('best_match');
-  const [ searchBusiness, setSearchBusiness ] = React.useState('');
-  const [ searchLocation, setSearchLocation ] = React.useState('');
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
 
-  const handleSort = (e) => {
-    setSortValue(e.target.value);
-  };
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
 
-  const handleBusinessChange = (e) => {
-    e.preventDefault();
-    setSearchBusiness(e.target.value);
-  };
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return 'active';
+    }
+    return '';
+  }
 
-  const handleLocationChange = (e) => {
-    e.preventDefault();
-    setSearchLocation(e.target.value);
-  };
+  handleSortByChange(sortByOption) {
+    this.setState({ sortBy: sortByOption });
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Sort by: ${sortValue}`);
-    console.log(`Search business: ${searchBusiness}`);
-    console.log(`Search location: ${searchLocation}`);
-    console.log('Submit button clicked');
-  };
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
 
-  return (
+  handleLocationChange(event) {
+    this.setState({ location: event.target.value });
+  }
 
-    <div className={styles.SearchBar}>
-      <Form>
-        <Container className="container-fluid d-flex flex-column justify-content-around align-items-center">
-          <ToggleButtonGroup type='radio' name='sortButtons' defaultValue={'best_match'} >
-            {sortOptions.map((radio, index) => (
-              <ToggleButton
-                key={index}
-                id={`radio-${index}`}
-                name="sortButtons"
-                variant='dark'
-                value={radio.value}
-                checked={sortValue === radio.value}
-                onChange={(e) => {
-                  handleSort(e)
-                }}
-              >
-                {radio.name}
-              </ToggleButton>
-            ))}
-          </ToggleButtonGroup>
-          <p>{sortValue}</p>
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
 
-          <Container className="container-fluid d-flex flex-lg-row flex-column">
-            <div className="p-lg-3 p-2 flex-grow-1">
-              <Form.Control 
-                className='form-control-lg' 
-                name='search' 
-                type="text" 
-                value={searchBusiness}
-                placeholder="Search businesses"
-                onChange={handleBusinessChange}
-              />
-            </div>
-            <div className="p-lg-3 p-2 flex-grow-1">
-            <Form.Control 
-                className='form-control-lg' 
-                name='search' 
-                type="text" 
-                value={searchLocation}
-                placeholder="Search businesses"
-                onChange={handleLocationChange}
-              />
+    event.preventDefault();
+  }
+
+  renderSortByOptions() {
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (
+
+        <ToggleButton
+          className={this.getSortByClass(sortByOptionValue)}
+             key={sortByOptionValue}
+          id={`radio-${sortByOptionValue}`}
+          name="sortButtons"
+          variant='dark'
+          value={sortByOptionValue}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >
+          {sortByOption}
+        </ToggleButton>
+
+      );
+    });
+  }
+
+  render() {
+    return (
+
+      <div className="SearchBar">
+        <Form>
+          <Container className="container-fluid d-flex flex-column justify-content-around align-items-center">
+            <ToggleButtonGroup type='radio' name='sortButtons' defaultValue={'best_match'} >
+              {this.renderSortByOptions()}
+            </ToggleButtonGroup>
+            <p>{this.sortValue}</p>
+
+            <Container className="container-fluid d-flex flex-lg-row flex-column">
+              <div className="p-lg-3 p-2 flex-grow-1">
+                <Form.Control
+                  className='form-control-lg'
+                  name='search'
+                  type="text"
+                  value={this.searchBusiness}
+                  placeholder="Search businesses"
+                  onChange={this.handleBusinessChange}
+                />
               </div>
-            <div className="p-lg-3 p-2 flex-grow-1">
-              <Button 
-                className='form-control-lg' 
-                variant="primary" 
-                type="submit"
-                name='submit' 
-                onClick={handleSubmit}
-              >
-                Submit
-              </Button>
-            </div>
+              <div className="p-lg-3 p-2 flex-grow-1">
+                <Form.Control
+                  className='form-control-lg'
+                  name='search'
+                  type="text"
+                  value={this.searchLocation}
+                  placeholder="Location"
+                  onChange={this.handleLocationChange}
+                />
+              </div>
+              <div className="p-lg-3 p-2 flex-grow-1">
+                <Button
+                  className='form-control-lg'
+                  variant="primary"
+                  type="submit"
+                  name='submit'
+                  onClick={this.handleSubmit}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Container>
           </Container>
-        </Container>
-      </Form>
-    </div>
+        </Form>
+      </div>
 
-  );
+    );
+
+  }
 }
 
 export default SearchBar;
+
+// const SearchBar = (searchYelp) => {
+
+//   const [ sortValue, setSortValue ] = React.useState('best_match');
+//   const [ searchBusiness, setSearchBusiness ] = React.useState('');
+//   const [ searchLocation, setSearchLocation ] = React.useState('');
+
+//   const handleSort = (e) => {
+//     setSortValue(e.target.value);
+//   };
+
+//   const handleBusinessChange = (e) => {
+//     e.preventDefault();
+//     setSearchBusiness(e.target.value);
+//   };
+
+//   const handleLocationChange = (e) => {
+//     e.preventDefault();
+//     setSearchLocation(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     console.log(`Sort by: ${sortValue}`);
+//     console.log(`Search business: ${searchBusiness}`);
+//     console.log(`Search location: ${searchLocation}`);
+//     console.log('Submit button clicked');
+
+//     searchYelp(searchBusiness, searchLocation, sortValue);
+//     e.preventDefault();
+//   };
+
+//   return (
+
+//     <div className={styles.SearchBar}>
+//       <Form>
+//         <Container className="container-fluid d-flex flex-column justify-content-around align-items-center">
+//           <ToggleButtonGroup type='radio' name='sortButtons' defaultValue={'best_match'} >
+//             {sortOptions.map((radio, index) => (
+//               <ToggleButton
+//                 key={index}
+//                 id={`radio-${index}`}
+//                 name="sortButtons"
+//                 variant='dark'
+//                 value={radio.value}
+//                 checked={sortValue === radio.value}
+//                 onChange={(e) => {
+//                   handleSort(e)
+//                 }}
+//               >
+//                 {radio.name}
+//               </ToggleButton>
+//             ))}
+//           </ToggleButtonGroup>
+//           <p>{sortValue}</p>
+
+//           <Container className="container-fluid d-flex flex-lg-row flex-column">
+//             <div className="p-lg-3 p-2 flex-grow-1">
+//               <Form.Control 
+//                 className='form-control-lg' 
+//                 name='search' 
+//                 type="text" 
+//                 value={searchBusiness}
+//                 placeholder="Search businesses"
+//                 onChange={handleBusinessChange}
+//               />
+//             </div>
+//             <div className="p-lg-3 p-2 flex-grow-1">
+//             <Form.Control 
+//                 className='form-control-lg' 
+//                 name='search' 
+//                 type="text" 
+//                 value={searchLocation}
+//                 placeholder="Location"
+//                 onChange={handleLocationChange}
+//               />
+//               </div>
+//             <div className="p-lg-3 p-2 flex-grow-1">
+//               <Button 
+//                 className='form-control-lg' 
+//                 variant="primary" 
+//                 type="submit"
+//                 name='submit' 
+//                 onClick={handleSubmit}
+//               >
+//                 Submit
+//               </Button>
+//             </div>
+//           </Container>
+//         </Container>
+//       </Form>
+//     </div>
+
+//   );
+// }
+
+// export default SearchBar;
