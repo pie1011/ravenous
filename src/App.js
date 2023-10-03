e072423deecb0a177b438fa93d8f65326df922ab
Katharine Harshman <katieharshman1011@gmail.com>
2023-10-02 20:54:41 -0700

    another try for yelp api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
-import data from './data.json';
+import Yelp from './utils/Yelp';
 
 function App() {
+
+  const [businesses, setBusinesses] = React.useState([])
+
+  const searchYelp = (term, location, sortBy) => {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      setBusinesses(businesses);
+    });
+  }
+
   return (
     <div className="App">
       <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList data={data} />
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,19 +14,37 @@ const sortOptions = [
 
 const SearchBar = () => {
 
-  const [ radioValue, setRadioValue ] = React.useState('best_match');
+  const [ sortValue, setSortValue ] = React.useState('best_match');
+  const [ searchBusiness, setSearchBusiness ] = React.useState('');
+  const [ searchLocation, setSearchLocation ] = React.useState('');
 
   const handleSort = (e) => {
-    setRadioValue(e.target.value);
+    setSortValue(e.target.value);
   };
-  console.log(radioValue);
+
+  const handleBusinessChange = (e) => {
+    e.preventDefault();
+    setSearchBusiness(e.target.value);
+  };
+
+  const handleLocationChange = (e) => {
+    e.preventDefault();
+    setSearchLocation(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Sort by: ${sortValue}`);
+    console.log(`Search business: ${searchBusiness}`);
+    console.log(`Search location: ${searchLocation}`);
+    console.log('Submit button clicked');
+  };
 
   return (
 
     <div className={styles.SearchBar}>
       <Form>
         <Container className="container-fluid d-flex flex-column justify-content-around align-items-center">
-
           <ToggleButtonGroup type='radio' name='sortButtons' defaultValue={'best_match'} >
             {sortOptions.map((radio, index) => (
               <ToggleButton
@@ -35,7 +53,7 @@ const SearchBar = () => {
                 name="sortButtons"
                 variant='dark'
                 value={radio.value}
-                checked={radioValue === radio.value}
+                checked={sortValue === radio.value}
                 onChange={(e) => {
                   handleSort(e)
                 }}
@@ -44,17 +62,37 @@ const SearchBar = () => {
               </ToggleButton>
             ))}
           </ToggleButtonGroup>
-          <p>{radioValue}</p>
+          <p>{sortValue}</p>
 
           <Container className="container-fluid d-flex flex-lg-row flex-column">
             <div className="p-lg-3 p-2 flex-grow-1">
-              <Form.Control name='search' className='form-control-lg' type="text" placeholder="Search businesses" />
+              <Form.Control 
+                className='form-control-lg' 
+                name='search' 
+                type="text" 
+                value={searchBusiness}
+                placeholder="Search businesses"
+                onChange={handleBusinessChange}
+              />
             </div>
             <div className="p-lg-3 p-2 flex-grow-1">
-              <Form.Control name='location' className='form-control-lg' type="text" placeholder="Where?" />
-            </div>
+            <Form.Control 
+                className='form-control-lg' 
+                name='search' 
+                type="text" 
+                value={searchLocation}
+                placeholder="Search businesses"
+                onChange={handleLocationChange}
+              />
+              </div>
             <div className="p-lg-3 p-2 flex-grow-1">
-              <Button name='submit' className='form-control-lg' variant="primary" type="submit">
+              <Button 
+                className='form-control-lg' 
+                variant="primary" 
+                type="submit"
+                name='submit' 
+                onClick={handleSubmit}
+              >
                 Submit
               </Button>
             </div>
@@ -62,6 +100,7 @@ const SearchBar = () => {
         </Container>
       </Form>
     </div>
+
   );
 }
 
