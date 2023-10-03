94b156af33293113a475effacfa1aaaad3540a02
Katie <pie10101011@gmail.com>
2023-10-03 22:24:00 +0000

    refactor App and BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,27 +1,20 @@
-import React from "react";
-import Business from "../Business/Business";
+import React from 'react';
+import './BusinessList.module.css';
 
-const BusinessList = (MockData) => {
-  return (
-    <div className="d-flex flex-row flex-wrap justify-content-around mb-3">
-      {MockData.data.map((business) => {
-        return (
-          <Business
-            key={business.id}
-            imageSrc={business.imageSrc}
-            name={business.name}
-            address={business.address}
-            city={business.city}
-            state={business.state}
-            zipCode={business.zipCode}
-            category={business.category}
-            rating={business.rating}
-            reviewCount={business.reviewCount}
-          />
-        );
-      })}
-    </div>
-  );
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} key={business.id} />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
